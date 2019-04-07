b52ab6e9f68460f41a2f22e5ed091f103ac5d6ec
Sung Jun Hong <sung.j.hong93@gmail.com>
2019-04-07 14:07:47 -0700

    Fix: Fixed ambiguous name

--- src/Books.js
+++ src/Books.js
@@ -13,7 +13,7 @@ class Books extends Component {
 					<option value="move" disabled>Move to...</option>
 					<option value="currentlyReading">Currently Reading</option>
 					<option value="wantToRead">Want to Read</option>
-					<option value="read">Read</option>
+					<option value="read">Finished Reading</option>
 					<option value="none">None</option>
 				  </select>
 				</div>
